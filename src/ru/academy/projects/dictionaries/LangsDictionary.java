f4d66cf9f91c489801ab06d098321f8112514048
Lisin_sw23@top-academy.ru <Lisin_sw23@top-academy.ru>
2025-12-27 11:04:49 +1000

    Add list lookup WordClass Done

--- src/ru/academy/projects/dictionaries/LangsDictionary.java
+++ src/ru/academy/projects/dictionaries/LangsDictionary.java
@@ -1,12 +1,12 @@
 package ru.academy.projects.dictionaries;
 
 import java.lang.reflect.MalformedParameterizedTypeException;
-import java.util.HashMap;
-import java.util.Map;
-import java.util.Scanner;
+import java.util.*;
+
+//import ru.academy.projects.dictionaries.WordTranslationField;
 
 public class LangsDictionary {
-    private final Map<String, String> dict = new HashMap<>();
+    private final Map<String, WordTranslationField> dict = new TreeMap<>();
     private final Scanner scanner = new Scanner(System.in);
 
     static void main(String[] args) {
@@ -15,14 +15,18 @@ public class LangsDictionary {
 
     private void run(){
         System.out.println("Словарь");
-        System.out.println("Команды: -a слово перевод | слово | -q");
+        System.out.println("Команды: -a слово перевод | слово | -list | -top | -q");
 
 
         String input;
 
         while (!(input = scanner.nextLine().trim()).equals("-q")) {
-            if (input.startsWith("-a ")){
+            if (input.startsWith("-a ")) {
                 add(input);
+            } else if (input.equals("-list")) {
+                list();
+            } else if (input.equals("-top")){
+                showTop();
             } else if (!input.isEmpty()){
                 lookup(input);
             }
@@ -34,22 +38,40 @@ public class LangsDictionary {
     private void add(String cmd){
         String[] parts = cmd.substring(3).split("\\s+", 2);
         if (parts.length < 2){
-            System.out.println("Формат: -a слово перевод");
+            System.out.println("Формат: -a слово перевод1, перевод2");
             return;
         }
         String word = parts[0].toLowerCase();
-        String tr = parts[1];
-        dict.put(word, tr);
-        System.out.println("Добавлено: " + word + " -> " + tr);
+        WordTranslationField el = dict.computeIfAbsent(word, k -> new WordTranslationField());
+        for (String tr : parts[1].split(",")){
+            el.addTranslation(tr);
+        }
+        System.out.println("Добавлено: " + word + " -> " + el);
     }
 
     private void lookup(String word){
-        String tr = dict.get(word.toLowerCase());
-        if(tr != null){
-            System.out.println(word + " -> " + tr);
+        WordTranslationField el = dict.get(word.toLowerCase());
+        if(el != null){
+            el.requestsCount++;
+            System.out.println(word + " -> " + el);
         } else {
             System.out.println("Слово " + word + " не найдено.");
         }
+    }
+    private  void list(){
+        if(dict.isEmpty()){
+            System.out.println("Словарь пуст");
+            return;
+        }
+        dict.forEach((wd, tr) -> System.out.println(wd + " -> " + tr));
+    }
+
+    private void showTop(){
+        dict.entrySet().stream()
+                .filter(el -> el.getValue().requestsCount > 0)
+                .sorted(Map.Entry.<String, WordTranslationField>comparingByValue())
+                .limit(3)
+                .forEach(el -> System.out.println(el.getKey() + " -> " + el.getValue()));
     }
 }
 
---
+++ src/ru/academy/projects/dictionaries/WordTranslationField.java
@@ -0,0 +1,33 @@
+package ru.academy.projects.dictionaries;
+import  java.util.*;
+import java.util.ArrayList;
+
+public class WordTranslationField implements Comparable<WordTranslationField> {
+    final List<String> translations = new ArrayList<>();
+    int requestsCount = 0;
+
+    public void addTranslation(String tr){
+        String clean = tr.trim().toLowerCase();
+
+        if(!clean.isEmpty() && !translations.contains(clean)) {
+            translations.add(clean);
+        }
+
+    }
+
+    @Override
+    public String toString() {
+        return "[" +
+                String.join(", " + translations) +
+                "] (использований: " +
+                requestsCount + ")";
+    }
+
+    @Override
+    // чтобы работала эта запись: .sorted(Map.Entry.<String, WordTranslationField>comparingByValue())
+    public int compareTo(WordTranslationField other) {
+        //сортируем по убыванию requestsCount: самые популярные - выше
+        // Integer.compare(other, this) -> для обратноо порядка
+        return Integer.compare(other.requestsCount, this.requestsCount);
+    }
+}
